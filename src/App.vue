61e94d6a333d22928a1b2150765030425cb193ee
sarahelshark <sarahelsh183@gmail.com>
2024-02-09 14:29:54 +0100

    loop in array di oggetti

--- src/App.vue
+++ src/App.vue
@@ -8,16 +8,33 @@ export default{
       movie:"",
       show: true,
       films:['Matrix','Avatar','Drive'],
-    }
+      users:[
+        {
+          id: 1 ,
+          name: "giuseppe",
+        },
+        {
+          id: 2 ,
+          name: "luca",
+        },
+        {
+          id: 3,
+          name: "isabella",
+        }
+      ]
+     }
   },
   methods:{
     greetings(){
       console.log( " ciao" , this.message );
      document.getElementById('emptyEl').innerHTML += `ciao ${this.message}`
     }
+}
 
 }
-}
+  
+
+  
 </script>
 
 <template>
@@ -31,9 +48,17 @@ export default{
    <p v-show="show">il film inserito: {{ movie }}</p>
    <p id="emptyEl"></p>
 
+   <!--loop in array di stringhe -->
    <ul>
-    <li v-for="film in films">
+    <li v-for="film in films"> 
        {{ film }}
+    </li>
+   </ul>
+
+   <!--loop in array of object-->
+   <ul>
+    <li v-for="user in users"> 
+       {{ user.name }} : {{ user.id }}
     </li>
    </ul>
   </div>
